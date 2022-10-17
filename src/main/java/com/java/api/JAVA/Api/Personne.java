0550e761f85a261161f92694c3274653cb5ae7ad
hugolanzafameynov <hugo.lanzafame@ynov.com>
2022-10-17 14:21:03 +0200

    Ajout de données java

---
+++ src/main/java/com/java/api/JAVA/Api/Personne.java
@@ -0,0 +1,15 @@
+package com.java.api.JAVA.Api;
+
+public class Personne {
+    String nom;
+    String prenom;
+    int age;
+    String resume;
+
+    public Personne(String prenom, String nom, int age, String resume){
+        this.nom=nom;
+        this.prenom=prenom;
+        this.age=age;
+        this.resume=resume;
+    }
+}
