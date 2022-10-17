76b7f1f17702df747518f3b8dc382a62e21d3b6b
MikeXXI <mike.dje@outlook.fr>
2022-10-17 12:02:05 +0200

    first commit

---
+++ src/test/java/com/java/api/JAVA/Api/JavaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.api.JAVA.Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
